16f18b5a5b4c15e86e79507a690e803108249e35
DATK <pasha.dark.dog@gmail.com>
2023-06-22 21:25:25 +0300

    m

--- Server/srv_start.bat
+++ Server/srv_start.bat
@@ -2,6 +2,7 @@
 Title Server running
 start /B python Server.py
 start /B python backup.py
-start "" python cmd.py
+:start "" python cmd.py
+start "" ngrok.exe http 5000
 :or start /B
 :/MIN to no window
